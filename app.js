0512511e2224a787bce7f5638f128f60ddd05053
FireRabbit <dohyu@naver.com>
2019-05-09 18:27:40 +0900

    change color

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
+
 canvas.width = 700;
 canvas.height = 700;
 
-const ctx = canvas.getContext('2d');
 ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
+
+
 
 let painting = false;
 
@@ -29,17 +33,15 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
-}
-
-function onMouseUp(event) {
-    stopPainting();
+function handleColorClick(event) {
+    ctx.strokeStyle = event.target.style.backgroundColor;
 }
 
 if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mousedown', onMouseDown);
-    canvas.addEventListener('mouseup', onMouseUp);
+    canvas.addEventListener('mousedown', startPainting);
+    canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
